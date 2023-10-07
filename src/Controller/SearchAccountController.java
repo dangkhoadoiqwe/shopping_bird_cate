a12c017be3d9ab930fd92027a01b380f4a7fc5fa
Quang1012 <135351309+Quang1012@users.noreply.github.com>
2023-10-07 21:40:55 +0700

    Create SearchAccountController.java

---
+++ src/Controller/SearchAccountController.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller;
+
+import DAO.AccountDao;
+import DTO.Account;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author PC
+ */
+@WebServlet(name = "SearchAccountController", urlPatterns = {"/SearchAccountController"})
+public class SearchAccountController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            String txtSearch = request.getParameter("txtSearch");
+            String searchBy = request.getParameter("searchBy");
+            String isSearch = request.getParameter("isSearch");
+            if (!txtSearch.isEmpty() && !txtSearch.isEmpty()) {
+                ArrayList<Account> accList = AccountDao.findAccByOption(txtSearch, searchBy);
+                if (accList != null && !accList.isEmpty()) {
+                    request.setAttribute("searchList", accList);
+                    request.setAttribute("lastSearchValue", txtSearch);
+                    request.setAttribute("searchByValue", searchBy);
+                    request.setAttribute("isSearch", isSearch);
+                     request.setAttribute("allAcc", accList);
+                }else{
+                    request.setAttribute("mess", "Not found!");
+                }
+            } else {
+                request.setAttribute("mess", "Do not leave blank!");
+            }
+          request.getRequestDispatcher("admin/allCus.jsp").forward(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(SearchAccountController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
